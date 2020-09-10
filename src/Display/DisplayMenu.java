414d41985e716b135f18fa9ecf26a5648be51199
Christian Christensen <christian@skjern-net.dk>
2020-09-10 09:19:10 +0200

    Remove TODO

--- src/Display/DisplayMenu.java
+++ src/Display/DisplayMenu.java
@@ -10,9 +10,6 @@ public class DisplayMenu implements iDisplay {
     }
 
     @Override
-    public void printView() {
-        // TODO Auto-generated method stub
-
-    }
+    public void printView() {}
     
 }
--- src/Display/DisplayOpretInteresser.java
+++ src/Display/DisplayOpretInteresser.java
@@ -3,9 +3,6 @@ package Display;
 public class DisplayOpretInteresser implements iDisplay {
 
     @Override
-    public void printView() {
-        // TODO Auto-generated method stub
-
-    }
+    public void printView() {}
     
 }

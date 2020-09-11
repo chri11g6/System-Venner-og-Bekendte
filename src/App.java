f199b73ccadb01b056d1aaf74fe757d865fdcb0d
Christian Christensen <christian@skjern-net.dk>
2020-09-11 15:37:16 +0200

    🚚 Fyldte main indehold til en ny class Don

--- src/App.java
+++ src/App.java
@@ -1,49 +1,51 @@
 import DataType.Global;
 import DataType.Interesser;
-import Display.DisplayMenu;
-import Display.iDisplay;
-import page.OpretPersonModul;
-import page.ViewModul;
+import page.MenuModul;
 import page.iPageModul;
 
 public class App {
     public static void main(String[] args) throws Exception {
         load();
-        boolean isRun = true;
 
-        iDisplay display = new DisplayMenu();
+        iPageModul menu = new MenuModul();
 
-        iPageModul opretPersonModul = new OpretPersonModul();
-        iPageModul viewModul = new ViewModul();
+        menu.run();
 
-        do {
+        // boolean isRun = true;
 
-            String key = display.getInputString();
+        // iDisplay display = new DisplayMenu();
 
-            switch (key) {
-                case "h":
-                case "help":
-                    display.printHelp();
-                    break;
-                case "opret":
-                    opretPersonModul.run();
-                    break;
-                case "view":
-                    viewModul.run();
-                    break;                
-                case "pwd":
-                    System.out.println("Du er på menu'en");
-                    break;                
-                case "q":
-                case "exit":
-                    isRun = false;
-                    break;
-                default:
-                    System.out.println("'h' eller 'help' For at få hjælp");
-                    break;
-            }
+        // iPageModul opretPersonModul = new OpretPersonModul();
+        // iPageModul viewModul = new ViewModul();
 
-        } while (isRun);
+        // do {
+
+        //     String key = display.getInputString();
+
+        //     switch (key) {
+        //         case "h":
+        //         case "help":
+        //             display.printHelp();
+        //             break;
+        //         case "opret":
+        //             opretPersonModul.run();
+        //             break;
+        //         case "view":
+        //             viewModul.run();
+        //             break;                
+        //         case "pwd":
+        //             System.out.println("Du er på menu'en");
+        //             break;                
+        //         case "q":
+        //         case "exit":
+        //             isRun = false;
+        //             break;
+        //         default:
+        //             System.out.println("'h' eller 'help' For at få hjælp");
+        //             break;
+        //     }
+
+        // } while (isRun);
 
     }
 
---
+++ src/page/MenuModul.java
@@ -0,0 +1,49 @@
+package page;
+
+import Display.DisplayMenu;
+import Display.iDisplay;
+
+public class MenuModul implements iPageModul {
+
+    private iDisplay display = new DisplayMenu();
+
+    private iPageModul opretPersonModul = new OpretPersonModul();
+    private iPageModul viewModul = new ViewModul();
+
+
+    @Override
+    public void run() {
+        boolean isRun = true;
+
+        do {
+
+            String key = display.getInputString();
+
+            switch (key) {
+                case "h":
+                case "help":
+                    display.printHelp();
+                    break;
+                case "opret":
+                    opretPersonModul.run();
+                    break;
+                case "view":
+                    viewModul.run();
+                    break;                
+                case "pwd":
+                    System.out.println("Du er på menu'en");
+                    break;                
+                case "q":
+                case "exit":
+                    isRun = false;
+                    break;
+                default:
+                    System.out.println("'h' eller 'help' For at få hjælp");
+                    break;
+            }
+
+        } while (isRun);
+
+    }
+    
+}

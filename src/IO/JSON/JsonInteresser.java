32e6d6a449a25f3d21fa241de1ff40e025681512
Christian Christensen <christian@skjern-net.dk>
2020-09-14 09:22:03 +0200

    push to

---
+++ src/IO/JSON/JsonInteresser.java
@@ -0,0 +1,17 @@
+package IO.JSON;
+
+import DataType.Interesser;
+
+public class JsonInteresser {
+
+    public static String encode(Interesser data) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void decode(String data) {
+        // TODO Auto-generated method stub
+
+    }
+    
+}

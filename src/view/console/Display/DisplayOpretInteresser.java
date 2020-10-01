11f0dda183228a8aabfbf6dc86dd295a5da74f1a
Christian Christensen <christian@skjern-net.dk>
2020-10-01 16:05:10 +0200

    Hard Reset

--- src/view/console/Display/DisplayOpretInteresser.java
+++
@@ -1,9 +0,0 @@
-package view.console.display;
-
-public class DisplayOpretInteresser implements iDisplay {
-
-	@Override
-	public void printView() {
-	}
-
-}
--- src/view/console/page/iPageModul.java
+++
@@ -1,5 +0,0 @@
-package view.console.page;
-
-public interface iPageModul {
-	public void run();
-}
